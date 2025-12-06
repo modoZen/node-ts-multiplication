109598084d18a8d473caa9bf55aed0721c238bbf
Max Diaz <mdiazn@uni.pe>
2025-12-06 11:01:25 -0500

    add yarg plugin

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,5 @@
-// import { mkdirSync, writeFileSync } from "fs";
+import { yarg } from "./config/plugin/args.plugin";
 
-import { mkdir, writeFile } from "fs/promises";
+console.log(process.argv);
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const base = 5;
-let template = `===================\n    Tabla del ${base}    \n===================`;
-
-numbers.forEach((number) => {
-  const newLine = `${base} x ${number} = ${base * number}`;
-  template = `${template}\n${newLine}`;
-});
-
-console.log(template);
-
-const folder = "outputs";
-
-// const outputPath = resolve(process.cwd(), folder, `table-${base}.txt`);
-
-const outputPath = `${folder}/table-${base}.txt`;
-
-const saveFile = async () => {
-  await mkdir(folder, { recursive: true });
-  await writeFile(outputPath, template);
-  console.log("carga exitosa de tabla");
-};
-
-saveFile();
-
-// mkdirSync(folder, { recursive: true });
-// writeFileSync(outputPath, template);
-// console.log("carga exitosa de tabla");
+console.log(yarg);
