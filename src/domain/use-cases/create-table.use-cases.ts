e49070759f6e1e7fc0bae0fcd3cd6a52d4025fcb
Max Diaz <mdiazn@uni.pe>
2025-12-07 12:15:09 -0500

    add CreateTable use cases

---
+++ src/domain/use-cases/create-table.use-cases.ts
@@ -0,0 +1,25 @@
+export interface CreatableUseCase {
+  execute: (options: CreateTableOptions) => string;
+}
+
+interface CreateTableOptions {
+  base: number;
+  limit?: number;
+}
+
+export class CreateTable implements CreatableUseCase {
+  constructor() {
+    /**
+     * DI - Dependency Injection
+     */
+  }
+
+  execute({ base, limit = 10 }: CreateTableOptions) {
+    let template = "";
+    for (let number = 1; number <= limit; number++) {
+      const newLine = `${base} x ${number} = ${base * number}`;
+      template += `${newLine}\n`;
+    }
+    return template;
+  }
+}
